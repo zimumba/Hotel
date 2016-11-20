baa31843be9785b75db99aa241e1f56ad85241ee
antonyo <toshka.ol@gmail.com>
2016-11-20 11:29:46 +0200

    Initial Commit

---
+++ Hotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hotel
+//
+//  Created by Antonyo on 11/20/16.
+//  Copyright © 2016 aoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hotel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hotel
+//
+//  Created by Antonyo on 11/20/16.
+//  Copyright © 2016 aoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
